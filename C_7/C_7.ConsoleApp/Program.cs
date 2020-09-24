efb9c6c76f8e0618603130ab33adb7c9a73278e8
francisco <franciscogaletti@yahoo.com>
2020-09-23 22:00:36 -0500

    Linq v_1

--- C_7/C_7.ConsoleApp/Program.cs
+++ C_7/C_7.ConsoleApp/Program.cs
@@ -20,22 +20,107 @@ namespace C_7.ConsoleApp
         {
             Console.Clear();
             Console.WriteLine("Choose an option:");
-            Console.WriteLine("1)  ");
-            Console.WriteLine("2) ");
-          
+            Console.WriteLine("1) Simple query Lambda ");
+            Console.WriteLine("2)  Simple Query");
+            Console.WriteLine("3)  chaining");
+            Console.WriteLine("4)  Operators");
+            Console.WriteLine("5)  reevaluation");
             Console.WriteLine("99) EXIT");
             Console.Write("\r\nSelect an option: ");
 
+            string[] names = { "Tom", "Dick", "Harry", "Mary", "Jay" };
             switch (Console.ReadLine())
             {
                 case "1":
+                  
+                    IEnumerable<string> filteredNames = names.Where(n => n.Length >= 4);
+
+                    foreach (var name in filteredNames)
+                    {
+                        Console.WriteLine(name);
+                    }
 
                     Console.WriteLine("finishing processing");
                     Console.ReadKey();
                     return true;
 
                 case "2":
-                    Console.WriteLine("Finishing Processing");
+                    // query sintax 1
+                  
+                    IEnumerable<string> filteredNames2 = from n in names
+                                                         where n.Length > 2
+                                                         select n;
+
+                    foreach (var name in filteredNames2)
+                    {
+                        Console.WriteLine(name);
+                    }
+
+                    Console.WriteLine("finishing simple query");
+                    Console.ReadKey();
+                    return true;
+
+                case "3":
+                    // chaining queries
+                    IEnumerable<string> query = names
+                        .Where(n => n.Length >= 4)
+                        .OrderBy(n => n.Length)
+                        .Select(n => n.ToUpper());
+
+                    foreach (var name in query)
+                    {
+                        Console.WriteLine(name);
+                    }
+
+                    Console.WriteLine("finishing chaining queries");
+                    Console.ReadKey();
+                    return true;
+
+                case "4":
+                    // STANDAR QUERY OPERATORS -> utilize
+                    // generic FUNC delegates.
+                    // operators
+                    int[] numbers = { 10, 9, 8, 7, 6 };
+
+                    int secondNumber = numbers.ElementAt(1);
+                    int secondLowest = numbers.OrderBy(n => n).Skip(1).First(); // 7
+                    int count = numbers.Count();
+                    int min = numbers.Min();
+
+                    bool hasNumberNine = numbers.Contains(9);
+                    bool hasMoreThanZero = numbers.Any();
+                    bool hasAnOddElement = numbers.Any(n => n % 2 != 0);
+
+
+                    Console.WriteLine("finishing chaining queries");
+                    Console.ReadKey();
+                    return true;
+
+                case "5":
+                    // REEVALUATION / DEFERRED QUERIES
+                    var number = new List<int>() { 1, 2 };
+
+                    IEnumerable<int> query5 = number.Select(n => n * 10);
+
+                    List<int> listTens = number.Select(n => n * 10).ToList();
+
+                    // clear
+                    foreach (int n in query5)
+                    {
+                        Console.WriteLine(n + "|"); //NOTHING
+                    }
+
+                    number.Clear();
+
+                    foreach (int n in query5)
+                    {
+                        Console.WriteLine(n + "|");
+                    }
+
+                    // No Reevaluation
+                    Console.WriteLine(listTens.Count);
+
+                    Console.WriteLine("finishing REEVALUATION queries");
                     Console.ReadKey();
                     return true;
 
@@ -46,6 +131,44 @@ namespace C_7.ConsoleApp
                     return true;
             }
         }
+    }
 
+    public static class MyExtensions
+    {
+        public static int WordCount(this String str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' },
+                             StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static IEnumerable<TSource> Where3<TSource>(this IEnumerable<TSource> source
+                                                        )
+        {
+            List<TSource> tempList = new List<TSource>();
+            foreach (var item in source)
+            {
+                if (item is string && item.ToString().Length < 4)
+                {
+
+                    tempList.Add(item);
+                }
+            }
+            return tempList;
+        }
+
+        public static IEnumerable<TSource> Where2<TSource>(this IEnumerable<TSource> source,
+                                                         Func<TSource, bool> predicate)
+        {
+            List<TSource> tempList = new List<TSource>();
+            foreach (var item in source)
+            {
+                if (item is string && item.ToString().Length < 4)
+                {
+
+                    tempList.Add(item);
+                }
+            }
+            return null;
+        }
     }
 }
