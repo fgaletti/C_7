6abde0ed597f3ec007fcf10842b4ce38d6230d12
francisco <franciscogaletti@yahoo.com>
2020-10-15 23:51:49 -0500

    Delegate 1

---
+++ C_7/C_7.ConsoleApp/DelegateClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_7.ConsoleApp
+{
+    public class DelegateClass
+    {
+        public delegate int Trasformer(int x); // case 1
+        public static int Square(int x)
+        {
+            return x * x;
+        }
+
+        public static void Transform(int[] values, Trasformer t)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                values[i] = t(values[i]);
+            }
+        }
+
+        public static void UpdateRefVal(ref int value)
+        {
+            value = 88;
+        }
+    }
+}
--- C_7/C_7.ConsoleApp/Program.cs
+++ C_7/C_7.ConsoleApp/Program.cs
@@ -8,6 +8,9 @@ namespace C_7.ConsoleApp
 {
     class Program
     {
+        delegate int Trasformer(int x); // case 1
+
+
         static void Main(string[] args)
         {
             bool showMenu = true;
@@ -20,8 +23,9 @@ namespace C_7.ConsoleApp
         {
             Console.Clear();
             Console.WriteLine("Choose an option:");
-            Console.WriteLine("1)  ");
-            Console.WriteLine("2) ");
+            Console.WriteLine("1) Delegate ");
+            Console.WriteLine("2) Plug IN Delegates ");
+            Console.WriteLine("3) ");
           
             Console.WriteLine("99) EXIT");
             Console.Write("\r\nSelect an option: ");
@@ -29,12 +33,34 @@ namespace C_7.ConsoleApp
             switch (Console.ReadLine())
             {
                 case "1":
+                    Trasformer t = DelegateClass.Square;
+                    int result = t(4);
+                    Console.WriteLine(result);
 
                     Console.WriteLine("finishing processing");
                     Console.ReadKey();
                     return true;
 
                 case "2":
+
+                    int[] values = { 1, 2, 3, 4 };
+                    DelegateClass.Transform(values, DelegateClass.Square);
+
+                    foreach (int i in values)
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                    // test value/ref
+                    int val = 77;
+                    DelegateClass.UpdateRefVal(ref val);
+                    Console.WriteLine(val);
+
+                    Console.WriteLine("finishing processing");
+                    Console.ReadKey();
+                    return true;
+
+                case "3":
                     Console.WriteLine("Finishing Processing");
                     Console.ReadKey();
                     return true;
